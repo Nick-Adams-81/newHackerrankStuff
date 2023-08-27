81dd05233142cb6fbc7a78b60cb8c027a4cd492e
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-27 14:08:39 -0700

    valid parens

---
+++ src/HackerrankAndLeetcodePractice/ValidParentheses.java
@@ -0,0 +1,17 @@
+package HackerrankAndLeetcodePractice;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean validParens(String str) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : str.toCharArray()) {
+            if(c == '{' || c == '[' || c == '(') stack.push(c);
+            else if(c == '}' && stack.peek() == '{') stack.pop();
+            else if(c == ']' && stack.peek() == '[') stack.pop();
+            else if(c == ')' && stack.peek() == '(') stack.pop();
+        }
+        return stack.isEmpty();
+    }
+}
