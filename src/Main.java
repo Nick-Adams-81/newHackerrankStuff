b3fadf25921eceac61fcaecb2ce77702ad2f4e68
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-27 13:32:07 -0700

    remove vowels using regex

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        RemoveVowels remove = new RemoveVowels();
         System.out.println(ReverseString.reverseString("hello"));
         System.out.println(ReverseString.reverseStr("world"));
         System.out.println(CountVowels.countVowels("hello world"));
+        System.out.println(remove.removeVowels("hello world"));
     }
 }
---
+++ src/RemoveVowels.java
@@ -0,0 +1,5 @@
+public class RemoveVowels {
+    public String removeVowels(String str) {
+        return str.replaceAll("[aeiou]", "");
+    }
+}
